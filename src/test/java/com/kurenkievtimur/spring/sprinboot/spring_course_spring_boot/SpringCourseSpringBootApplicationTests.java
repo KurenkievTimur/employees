9d1210192429621874c1f283a3e0bc9e17e5ef04
KurenkievTimur <ti-mur99@mail.ru>
2022-04-18 19:25:55 +0600

    Initial commit

---
+++ src/test/java/com/kurenkievtimur/spring/sprinboot/spring_course_spring_boot/SpringCourseSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kurenkievtimur.spring.sprinboot.spring_course_spring_boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCourseSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
